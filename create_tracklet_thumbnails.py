280ed3cb8739f132d4d2e1aa26fa3a202a60444c
efti-nile <efti.nile@gmail.com>
2020-06-29 16:18:13 +0400

    nice arguments

--- create_tracklet_thumbnails.py
+++ create_tracklet_thumbnails.py
@@ -1,29 +1,45 @@
 import os
 import glob
+import argparse
 
 import cv2
 import numpy as np
 
 
-OUT_HEIGHT = 12000  # px
-MAX_LEN = 12  # truncate tracklets to this length
+def main():
+    args = get_args()
 
-tracklet_images = []
-for tracklet_folder in glob.glob('data/13*'):
-    tracklet_images.append([
-        cv2.imread(img_filename) for img_filename in sorted(glob.glob(os.path.join(tracklet_folder, '*.jpg')))
-    ])
+    tracklet_images = []
+    for tracklet_folder in glob.glob(args['data']):
+        tracklet_images.append([
+            cv2.imread(img_filename) for img_filename in sorted(glob.glob(os.path.join(tracklet_folder, '*.jpg')))
+        ])
 
-height = OUT_HEIGHT // len(tracklet_images)
-width = height // 2
-thumbnail_size = (width, height)
+    print(f'tracklets found: {len(tracklet_images)}')
 
-tracklet_images = [[cv2.resize(img, thumbnail_size) for img in tracklet[:10]] for tracklet in tracklet_images]
+    height = args['height'] // len(tracklet_images)
+    width = height // 2
+    thumbnail_size = (width, height)
 
-out_image = np.zeros((OUT_HEIGHT, width * MAX_LEN, 3))
+    tracklet_images = [[cv2.resize(img, thumbnail_size) for img in tracklet[:10]] for tracklet in tracklet_images]
 
-for i, tracklet in enumerate(tracklet_images):
-    for j, image in enumerate(tracklet):
-        out_image[i * height:(i+1) * height, j * width:(j+1) * width, :] = image
+    out_image = np.zeros((args['height'], width * args['maxlen'], 3))
 
-cv2.imwrite('out.jpg', out_image)
+    for i, tracklet in enumerate(tracklet_images):
+        for j, image in enumerate(tracklet):
+            out_image[i * height:(i+1) * height, j * width:(j+1) * width, :] = image
+
+    cv2.imwrite(args['out'], out_image)
+
+
+def get_args():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("--data", required=True, help="a glob to pick tracklet folders")
+    ap.add_argument("--height", default=6000, help="output image height in pixels", type=int)
+    ap.add_argument("--maxlen", default=10, help="truncate long tracklets to this length", type=int)
+    ap.add_argument("--out", required=True, help="a glob to pick tracklet folders")
+    return vars(ap.parse_args())
+
+
+if __name__ == '__main__':
+    main()
